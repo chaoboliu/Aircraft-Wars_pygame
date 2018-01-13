27809784179bedc9ba7d809f97f8514c3ee1be20
chaoboliu <1310276776@qq.com>
2018-01-13 11:39:40 +0800

    safsdf

--- 积分系统3.1版.py
+++ 积分系统3.1版.py
@@ -15,12 +15,12 @@ def aa():
 			for k,v in i.items():
 				print("%s:%s"%(k,v))
 def	bb():
-		soso = input("如果按差键可以点击'enter'键退出到'功能管理'功能\n 请输入您要查询的名字:")
+		soso = input("请输入您要查询的名字:")
 		for dic in list:
 			if soso == dic["name"]:
 				print("名字:%s 积分:%d"%(dic["name"],dic["jifen"]))
 def cc():
-		name = input("如果按差键可以点击'enter'键退出到'功能管理'功能\n 请输入您要修改的名字:")
+		name = input("请输入您要修改的名字:")
 		for dic in list:
 			if dic["name"] == name:
 				name2 = input("请输入您要替换的新名字:")
